fa987ae57b9670e5bb8b6504264cd2ac338a5c42
Matthew Holmes <matthew.holmes111@gmail.com>
2024-01-24 17:16:53 +0000

    added a config class

---
+++ Infrastructure/ConfigManager.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class ConfigManager
+    {
+        public const String ConfigPath = "config.json";
+
+        public static Config? LoadConfig(String path = ConfigPath)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            String json = File.ReadAllText(path);
+
+            try
+            {
+                return JsonConvert.DeserializeObject<Config>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static bool UpdateConfig(Config newConfig)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(newConfig, Formatting.Indented);
+                File.WriteAllText(ConfigPath, json);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static Config GenerateDefaultConfig()
+        {
+            Config config = new Config();
+
+            config.FileStoreLocation = "Filestore/";
+
+            return config;
+        }
+    }
+}
