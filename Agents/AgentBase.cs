26c810cbbbfaed2165303089e95bd8b6227697cb
Matthew Holmes <matthew.holmes111@gmail.com>
2024-02-02 15:45:26 +0000

    added abstract base class for ai agents, and a dummy agent for testing

---
+++ Agents/AgentBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agents
+{
+    public abstract class AgentBase
+    {
+        public AgentBase()
+        {
+            PromptResponseHistory = new List<Tuple<String, String>>();
+        }
+
+        private object responseLock = new object();
+
+        public String GetResponse(String prompt)
+        {
+            String response;
+
+            lock (responseLock)
+            {
+                response = GetResponseCore(prompt);
+
+                PromptResponseHistory.Add(Tuple.Create(prompt, response));
+
+            }
+            return response;
+        }
+
+        protected abstract String GetResponseCore(String prompt);
+
+        public List<Tuple<String, String>> PromptResponseHistory { get; private set; }
+    }
+}
---
+++ Agents/DummyAgents/DummyTextDecompositionAgent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agents.DummyAgents
+{
+    public class DummyTextDecompositionAgent : AgentBase
+    {
+        protected override String GetResponseCore(String prompt)
+        {
+
+            int len = prompt.Length;
+
+            String response = len > 5 ? prompt.Substring(0, 5) : prompt;
+
+            int lhs = 5;
+            int rhs = 10;
+
+            while (rhs <= len)
+            {
+                response += '\n';
+                response += len > lhs + 5 ? prompt.Substring(lhs, 5) : prompt.Substring(lhs);
+                lhs = rhs;
+                rhs += 5;
+            }
+
+            return response;
+
+        }
+    }
+}
