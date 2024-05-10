1a97242c97d1e70472dfa829d1a35fd25c430c96
Matthew Holmes <matthew.holmes111@gmail.com>
2024-05-10 16:46:04 +0100

    rooting rework

--- Agents/UnitRootingAgent.cs
+++ Agents/UnitRootingAgent.cs
@@ -9,12 +9,12 @@ namespace Agents
 {
     public class UnitRootingAgent : OpenAIProcessingBase
     {
-        private static String systemMessage = "return the roots of the following words/phrases. You should remove inflection, converting verbs to the infinitive and nouns to the singular for example. Remove unecessary punctutation. Make unecessary uppercase lowercase, keep proper nouns, names, places with initial uppercase. Format \nword1\nword2 etc. Recombine names and compound words that have been split across lines. Do not omit any lines";
+        private static String systemMessage = "process the following lines to remove inflection, converting verbs to the infinitive and nouns to the singular for example. Use standard English capitalisation, proper nouns, names, surnames, locations, place names, acronyms MUST obey capitisation rules, with the initial letter capitalised, otherwise favour lowercase. Remove unecessary punctutation and possesive apostrophes. You MUST NOT omit lines. DO NOT combine lines. DO NOT split lines. The number of output and input lines MUST be the same. Format \nword1\nword2 etc.";
 
         public UnitRootingAgent(string apiKey) : base(apiKey)
         {
-            //ContinousParameter("Temperature").Value = 0.1;
-            //ContinousParameter("TopP").Value = 0.25;
+            ContinousParameter("Temperature").Value = 1.0; // below 1 tends to recombine lines
+            ContinousParameter("TopP").Value = 0.5;
             StringParameters["system"] = systemMessage;
         }
     }
---
+++ LearningExtraction/UnitRooter.cs
@@ -0,0 +1,54 @@
+﻿using Agents;
+using Agents.OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningExtraction
+{
+    public class UnitRooter
+    {
+        public int MaxVolumeToProcess { get; set; } = 200; // if given text larger than this, chunk it
+        public int JoinCharacterCount { get; set; } = 20;
+
+        public void SetTemperature(double temp)
+        {
+            if (Agent is OpenAIBase OpenAiAgent)
+            {
+                OpenAiAgent.ContinousParameter("Temperature").Value = temp;
+            }
+            else
+            {
+                throw new Exception("Can't set temperature on a non OpenAI agent");
+            }
+
+        }
+
+        public void SetTopP(double topP)
+        {
+            if (Agent is OpenAIBase OpenAiAgent)
+            {
+                OpenAiAgent.ContinousParameter("TopP").Value = topP;
+            }
+            else
+            {
+                throw new Exception("Can't set topP on a non OpenAI agent");
+            }
+
+        }
+
+        public AgentBase Agent { get; set; }
+
+        public async Task<TextDecomposition> RootUnits(TextDecomposition priorDecomposition)
+        {
+            if (priorDecomposition.Units == null)
+            {
+                throw new ArgumentException("No units to normalise");
+            }
+
+            return await DecompositionTransformer.ApplyAgent(Agent, priorDecomposition, MaxVolumeToProcess, JoinCharacterCount);
+        }
+    }
+}
