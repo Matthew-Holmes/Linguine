de0583b07bcb39c60f44c7777e7941014ac6e458
Matthew Holmes <matthew.holmes111@gmail.com>
2024-05-13 11:39:58 +0100

    started work on definition resolver class

--- LearningExtraction/DefinitionResolver.cs
+++ LearningExtraction/DefinitionResolver.cs
@@ -11,26 +11,15 @@ namespace LearningExtraction
     public class DefinitionResolver
     {
         private ExternalDictionary _dictionary;
-        private TextDecomposition _injectiveDecomposition;
-        private TextDecomposition _rootedDecomposition;
 
-        public DefinitionResolver(ExternalDictionary dictionary, TextDecomposition injectiveDecomposition, TextDecomposition rootedDecomposition)
+        public DefinitionResolver(ExternalDictionary dictionary)
         {
             _dictionary = dictionary;
-
-            if (injectiveDecomposition.Decomposition.Count != rootedDecomposition.Decomposition.Count)
-            {
-                throw new ArgumentException("rooted decomposition has different number of units to the injective decomposition");
-            }
-
-            if (injectiveDecomposition.Total.Text != rootedDecomposition.Total.Text)
-            {
-                throw new ArgumentException("decompositions' total texts differ");
-            }
-
-            _injectiveDecomposition = injectiveDecomposition;
-            _rootedDecomposition = rootedDecomposition;
-
+        }
+        
+        private List<List<DictionaryDefinition>> GetPossibleDefinitions(TextDecomposition td)
+        {
+            return td.Units.Select(u => _dictionary.TryGetDefinition(u.Text)).ToList();
         }
 
     }
