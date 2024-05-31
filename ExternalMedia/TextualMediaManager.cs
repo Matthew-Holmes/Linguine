13fbf0db2ed9802a559e6303c4430551a4f8abb1
Matthew Holmes <matthew.holmes111@gmail.com>
2024-05-31 17:50:31 +0100

    added a class to manage textual media and provide a facade of the database

---
+++ ExternalMedia/TextualMediaManager.cs
@@ -0,0 +1,60 @@
+﻿using Infrastructure;
+
+namespace Linguine
+{
+    public class TextualMediaManager
+    {
+        // TODO - tests
+        private LinguineDbContext _db;
+
+        public TextualMediaManager(LinguineDbContext linguine)
+        {
+            _db = linguine;
+        }
+
+        public void Add(TextualMedia tm)
+        {
+            if (AvailableTextualMediaNames().Contains(tm.Name))
+            {
+                throw new ArgumentException("already have a text of this name");
+            }
+
+            _db.TextualMedia.Add(tm);
+        }
+
+        public List<String> AvailableTextualMediaNames()
+        {
+            return _db.TextualMedia.Select(m => m.Name)
+                               .Distinct()
+                               .ToList();
+        }
+
+        public bool HaveMediaWithSameDescription(String description)
+        {
+            return _db.TextualMedia.Where(m => m.Description == description).Any();
+        }
+
+        public bool HaveMediaWithSameContent(String text)
+        {
+            return _db.TextualMedia.Where(m => m.Description == text).Any();
+        }
+
+        public TextualMedia? GetByName(string selectedText)
+        {
+            var possibilities = _db.TextualMedia.Where(m => m.Name == selectedText).ToList();
+    
+            if (possibilities is null || possibilities.Count == 0)
+            {
+                return null;
+            }
+
+            if (possibilities.Count > 1)
+            {
+                throw new Exception("multiple texts of same name found - database corrupted!");
+            }
+
+            return possibilities.First();
+
+        }
+    }
+}
