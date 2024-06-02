d615a826c4ace5d6a541f84002d8c0ffdcff7786
Matthew Holmes <matthew.holmes111@gmail.com>
2024-06-02 19:01:36 +0100

    added paging

--- Linguine/Tabs/TabViewModels/TextualMediaViewerViewModel.cs
+++ Linguine/Tabs/TabViewModels/TextualMediaViewerViewModel.cs
@@ -19,6 +19,7 @@ namespace Linguine.Tabs
     {
         public readonly int SessionID;
         private int _charsToDisplay = 1000;
+        private int _linesToDisplay = 20;
         private int _localCursor;
 
         private String _fullText;
@@ -66,13 +67,41 @@ namespace Linguine.Tabs
 
         private void Step(int pages)
         {
-            _localCursor += pages * _charsToDisplay;
+            // check don't exceed line limits
+            int lhs = Math.Max(Math.Min(_localCursor, _localCursor + pages * _charsToDisplay), 0);
+            int rhs = Math.Min(Math.Max(_localCursor, _localCursor + pages * _charsToDisplay), _fullText.Length);
+
+            List<String> linesMoved = _fullText.Substring(lhs, rhs - lhs).Split('\n').ToList();
+
+            if (linesMoved.Count > pages * _linesToDisplay)
+            {
+                if (pages < 0)
+                {
+                    linesMoved.Reverse();
+                }
+                int newStep = linesMoved.Take(Math.Abs(pages) * _linesToDisplay).Sum(s => s.Length);
+                _localCursor += Math.Sign(pages) * newStep;
+            }
+            else
+            {
+                _localCursor += pages * _charsToDisplay;
+            }
+
             _localCursor = Math.Min(_fullText.Length - _charsToDisplay, _localCursor);
             _localCursor = Math.Max(0, _localCursor);
 
             int windowEnd = Math.Min(_fullText.Length, _localCursor +  _charsToDisplay);
 
-            TextViewWindow = _fullText.Substring(_localCursor, windowEnd);
+            string tmp = _fullText.Substring(_localCursor, windowEnd - _localCursor);
+
+            List<String> tmpLines = tmp.Split('\n').ToList();
+
+            if (tmpLines.Count > _linesToDisplay)
+            {
+                tmp = String.Join('\n', tmpLines.Take(_linesToDisplay));
+            }
+
+            TextViewWindow = tmp;
 
             if (!_mainModel.CursorMoved(SessionID, _localCursor))
             {
