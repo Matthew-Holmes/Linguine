3db1a8241c79e1eee2faa96c0b5221d6ad4b55db
Matthew Holmes <matthew.holmes111@gmail.com>
2024-06-03 17:42:43 +0100

    added foreign key properties

--- Infrastructure/DataClasses/DefinitionTreeNode.cs
+++ Infrastructure/DataClasses/DefinitionTreeNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Infrastructure.DataClasses
 {
     public class DefinitionTreeNode
     {
+        [Key]
+        public int DatabasePrimaryKey { get; set; }
         public int IndexAtCurrentLevel { get; set; }
+        public int DefinitionKey { get; set; }
         public DictionaryDefinition? DictionaryDefinition { get; set; } // null if no definition here
+        public int ParentKey { get; set; }
         public DefinitionTreeNode? Parent { get; set; } // null if root node
     }
 }
--- Infrastructure/DataClasses/StatementDatabaseEntry.cs
+++ Infrastructure/DataClasses/StatementDatabaseEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,18 @@ namespace Infrastructure.DataClasses
     public class StatementDatabaseEntry
     {
         // since these will be numerous and data rich, use a minimal entry to store in the database
+        [Key]
+        public int DatabasePrimaryKey { get ; set; }
 
         // statement slice identification
         public TextualMedia Parent { get; set; }
+        public int ParentKey { get; set; }
         public int FirstCharIndex { get; set; }
         public int LastCharIndex { get; set; }
 
         // efficient storing of contextual information
         public StatementDatabaseEntry? Previous { get; set; } // null if the first statement of a piece of media
+        public int PreviousKey { get; set; }
         public List<String>? ContextCheckpoint { get; set; } // so don't have to replay all the way to root
         public List<int> ContextDeltaRemovalsDescendingIndex { get; set; }
         public List<Tuple<int, String>> ContextDeltaInsertionsDescendingIndex { get; set; }
@@ -25,6 +30,7 @@ namespace Infrastructure.DataClasses
         // compressed TextDecompositions
         public String HeadlessInjectiveDecompositionJSON { get; set; }
         public String HeadlessRootedDecompositionJSON { get; set; }
+        public int DefinitionTreeRootKey { get; set; }  
         public DefinitionTreeNode DefinitionTreeRoot { get; set; }
     }
 }
