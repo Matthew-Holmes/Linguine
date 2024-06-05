e605c3af83c137af881e19b42974baf134448fb7
Matthew Holmes <matthew.holmes111@gmail.com>
2024-06-05 13:47:02 +0100

    finished context delta tests

--- Tests_LearningExtraction/StatementDatabaseEntryFactoryTests.Context.cs
+++ Tests_LearningExtraction/StatementDatabaseEntryFactoryTests.Context.cs
@@ -1883,6 +1883,295 @@ namespace Tests_LearningExtraction
 
         }
 
+        [TestMethod]
+        public void FromStatement_TwoStatementsNoPreviousNoDecompositionContextSwapsFirstOne_CorrectDeltas()
+        {
+            TextualMedia parentText = new TextualMedia();
+            //-----------------000000000011111111112222222222333333333344444444445555555555666666666677777777778888888888
+            //-----------------012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+            parentText.Text = "no decomposition, and now something unrelated";
+
+            TextDecomposition injective1 = new TextDecomposition("no decomposition", null);
+            TextDecomposition rooted1 = new TextDecomposition("no decomposition", null);
+
+            Statement statement1 = new Statement(
+                parentText, 0, 15,
+                "no decomposition",
+                new List<string>
+                    {
+                        "in testing code",
+                        "used to test a factory method involving text broken into units"
+                    },
+                injective1, rooted1);
+
+            TextDecomposition injective2 = new TextDecomposition("and now something unrelated", null);
+            TextDecomposition rooted2 = new TextDecomposition("and now something unrelated", null);
+
+            Statement statement2 = new Statement(
+                parentText, 18, 44,
+                "and now something unrelated",
+                new List<string>
+                {
+                    "outer changed",
+                    "used to test a factory method involving text broken into units"
+                },
+                injective2, rooted2);
+
+            var ret = StatementDatabaseEntryFactory.FromStatements(
+                new List<Statement> { statement1, statement2 }, null);
+
+            var entry2 = ret[1].Item1;
+
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex.Count, 1);
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex[0], 0);
+
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex.Count, 1);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item1, 0);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item2, "outer changed");
+
+        }
+
+        [TestMethod]
+        public void FromStatement_TwoStatementsNoPreviousNoDecompositionContextSwapsMiddleOne_CorrectDeltas()
+        {
+            TextualMedia parentText = new TextualMedia();
+            //-----------------000000000011111111112222222222333333333344444444445555555555666666666677777777778888888888
+            //-----------------012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+            parentText.Text = "no decomposition, and now something unrelated";
+
+            TextDecomposition injective1 = new TextDecomposition("no decomposition", null);
+            TextDecomposition rooted1 = new TextDecomposition("no decomposition", null);
+
+            Statement statement1 = new Statement(
+                parentText, 0, 15,
+                "no decomposition",
+                new List<string>
+                    {
+                        "in testing code",
+                        "used to test a factory method involving text broken into units",
+                        "more context"
+                    },
+                injective1, rooted1);
+
+            TextDecomposition injective2 = new TextDecomposition("and now something unrelated", null);
+            TextDecomposition rooted2 = new TextDecomposition("and now something unrelated", null);
+
+            Statement statement2 = new Statement(
+                parentText, 18, 44,
+                "and now something unrelated",
+                new List<string>
+                {
+                    "in testing code",
+                    "specifically the context updating",
+                    "more context"
+
+                },
+                injective2, rooted2);
+
+            var ret = StatementDatabaseEntryFactory.FromStatements(
+                new List<Statement> { statement1, statement2 }, null);
+
+            var entry2 = ret[1].Item1;
+
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex.Count, 1);
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex[0], 1);
+
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex.Count, 1);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item1, 1);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item2, "specifically the context updating");
+
+        }
+
+        [TestMethod]
+        public void FromStatement_TwoStatementsNoPreviousNoDecompositionContextSwapsAll_CorrectDeltas()
+        {
+            TextualMedia parentText = new TextualMedia();
+            //-----------------000000000011111111112222222222333333333344444444445555555555666666666677777777778888888888
+            //-----------------012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+            parentText.Text = "no decomposition, and now something unrelated";
+
+            TextDecomposition injective1 = new TextDecomposition("no decomposition", null);
+            TextDecomposition rooted1 = new TextDecomposition("no decomposition", null);
+
+            Statement statement1 = new Statement(
+                parentText, 0, 15,
+                "no decomposition",
+                new List<string>
+                    {
+                        "in testing code",
+                        "used to test a factory method involving text broken into units",
+                        "more context"
+                    },
+                injective1, rooted1);
+
+            TextDecomposition injective2 = new TextDecomposition("and now something unrelated", null);
+            TextDecomposition rooted2 = new TextDecomposition("and now something unrelated", null);
+
+            Statement statement2 = new Statement(
+                parentText, 18, 44,
+                "and now something unrelated",
+                new List<string>
+                {
+                    "in more testing code",
+                    "specifically the context updating",
+                    "even more context"
+
+                },
+                injective2, rooted2);
+
+            var ret = StatementDatabaseEntryFactory.FromStatements(
+                new List<Statement> { statement1, statement2 }, null);
+
+            var entry2 = ret[1].Item1;
+
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex.Count, 3);
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex[0], 2);
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex[1], 1);
+            Assert.AreEqual(entry2.ContextDeltaRemovalsDescendingIndex[2], 0);
+
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex.Count, 3);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item1, 0);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[0].Item2, "even more context");
+
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[1].Item1, 0);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[1].Item2, "specifically the context updating");
+
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[2].Item1, 0);
+            Assert.AreEqual(entry2.ContextDeltaInsertionsDescendingIndex[2].Item2, "in more testing code");
+
+
+
+        }
+
+        [TestMethod]
+        public void FromStatement_MultipleStatementsNoPreviousNoDecompositionContextChanges_CorrectDeltas()
+        {
+            TextualMedia parentText = new TextualMedia();
+            //-----------------000000000011111111112222222222333333333344444444445555555555666666666677777777778888888888
+            //-----------------012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+            parentText.Text = "no decomposition, but now there are multiple statements, such as this, this one, and this";
+
+            TextDecomposition injective1 = new TextDecomposition("no decomposition", null);
+            TextDecomposition rooted1 = new TextDecomposition("no decomposition", null);
+
+            Statement statement1 = new Statement(
+                parentText, 0, 15,
+                "no decomposition",
+                new List<string>
+                    {
+                        "in testing code",
+                        "used to test a factory method involving text broken into units"
+                    },
+                injective1, rooted1);
+
+            TextDecomposition injective2 = new TextDecomposition("but now there are multiple statements", null);
+            TextDecomposition rooted2 = new TextDecomposition("but now there are multiple statements", null);
+
+            Statement statement2 = new Statement(
+                parentText, 18, 49,
+                "but now there are multiple statements",
+                new List<string>
+                    {
+                        "in testing code",
+                        "used to test a factory method involving text broken into units"
+                    },
+                injective2, rooted2);
+
+            TextDecomposition injective3 = new TextDecomposition("such as this", null);
+            TextDecomposition rooted3 = new TextDecomposition("such as this", null);
+
+            Statement statement3 = new Statement(
+                parentText, 57, 68,
+                "such as this",
+                new List<string>
+                    {
+                        "in testing code",
+                        "now testing changing context",
+                        "trailing off",
+                    },
+                injective3, rooted3);
+
+            TextDecomposition injective4 = new TextDecomposition("this one", null);
+            TextDecomposition rooted4 = new TextDecomposition("this one", null);
+
+            Statement statement4 = new Statement(
+                parentText, 71, 78,
+                "this one",
+                new List<string>
+                    {
+                        "inside a test statement",
+                        "testing changing context",
+                        "trailing off",
+                        "near the end",
+                    },
+                injective4, rooted4);
+
+            TextDecomposition injective5 = new TextDecomposition("and this", null);
+            TextDecomposition rooted5 = new TextDecomposition("and this", null);
+
+            Statement statement5 = new Statement(
+                parentText, 81, 88,
+                "and this",
+                new List<string>
+                {
+                },
+                injective5, rooted5);
+
+            var ret = StatementDatabaseEntryFactory.FromStatements(
+                new List<Statement> { statement1, statement2, statement3, statement4, statement5 }, null);
+
+            var entry1 = ret[0].Item1;
+            var entry2 = ret[1].Item1;
+            var entry3 = ret[2].Item1;
+            var entry4 = ret[3].Item1;
+            var entry5 = ret[4].Item1;
+
+            var insertions1 = entry1.ContextDeltaInsertionsDescendingIndex;
+            var insertions2 = entry2.ContextDeltaInsertionsDescendingIndex;
+            var insertions3 = entry3.ContextDeltaInsertionsDescendingIndex;
+            var insertions4 = entry4.ContextDeltaInsertionsDescendingIndex;
+            var insertions5 = entry5.ContextDeltaInsertionsDescendingIndex;
+
+            var removals1 = entry1.ContextDeltaRemovalsDescendingIndex;
+            var removals2 = entry2.ContextDeltaRemovalsDescendingIndex;
+            var removals3 = entry3.ContextDeltaRemovalsDescendingIndex;
+            var removals4 = entry4.ContextDeltaRemovalsDescendingIndex;
+            var removals5 = entry5.ContextDeltaRemovalsDescendingIndex;
+
+            Assert.AreEqual(insertions1.Count, 0);
+            Assert.AreEqual(removals1.Count, 0);
+
+            Assert.AreEqual(insertions2.Count, 0);
+            Assert.AreEqual(removals2.Count, 0);
+
+            Assert.AreEqual(removals3.Count, 1);
+            Assert.AreEqual(removals3[0], 1);
+            Assert.AreEqual(insertions3.Count, 2);
+            Assert.AreEqual(insertions3[0].Item1, 1);
+            Assert.AreEqual(insertions3[0].Item2, "trailing off");
+            Assert.AreEqual(insertions3[1].Item1, 1);
+            Assert.AreEqual(insertions3[1].Item2, "now testing changing context");
+
+
+            Assert.AreEqual(removals4.Count, 2);
+            Assert.AreEqual(removals4[0], 1);
+            Assert.AreEqual(removals4[1], 0);
+            Assert.AreEqual(insertions4.Count, 3);
+            Assert.AreEqual(insertions4[0].Item1, 1);
+            Assert.AreEqual(insertions4[0].Item2, "near the end");
+            Assert.AreEqual(insertions4[1].Item1, 0);
+            Assert.AreEqual(insertions4[1].Item2, "testing changing context");
+            Assert.AreEqual(insertions4[2].Item1, 0);
+            Assert.AreEqual(insertions4[2].Item2, "inside a test statement");
+
+            Assert.AreEqual(insertions5.Count, 0);
+            Assert.AreEqual(removals5.Count, 4);
+            Assert.AreEqual(removals5[0], 3);
+            Assert.AreEqual(removals5[1], 2);
+            Assert.AreEqual(removals5[2], 1);
+            Assert.AreEqual(removals5[3], 0);
+        }
+
 
     }
 }
