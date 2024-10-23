0e14c04e381768c9a73ebd83487c4ce1247d31c8
Matthew Holmes <matthew.holmes111@gmail.com>
2024-10-23 20:05:26 +0100

    added extra case in case decomposition

--- LearningExtraction/TextDecomposition/TextDecomposer.cs
+++ LearningExtraction/TextDecomposition/TextDecomposer.cs
@@ -8,6 +8,7 @@ using Agents;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure;
+using System.Text.RegularExpressions;
 
 namespace LearningExtraction
 {
@@ -21,6 +22,8 @@ namespace LearningExtraction
 
         public async Task<TextDecomposition> DecomposeText(String text, bool mustInject = true, bool mustBiject = false)
         {
+           
+
             if (text.Length > MaxVolumeToProcess)
             {
                 throw new ArgumentException("Text too long to process");
@@ -37,7 +40,17 @@ namespace LearningExtraction
                 return ret;
             }
 
-            // first attempt failed, use more powerful agent (e.g. higher spec LLM)
+            String text_stripped = Regex.Replace(text, @"\t|\n|\r", "");
+
+            // first attempt failed, maybe there were too many newlines?
+            // TODO - only do this if there is a high newline density
+            ret = await FromTextUsingAgent(text_stripped, StandardAgent);
+            if (MaintainsInvariants(ret))
+            {
+                return ret;
+            }
+
+            // that failed, use more powerful agent (e.g. higher spec LLM)
             ret = await FromTextUsingAgent(text, HighPerformanceAgent);
 
             if (MaintainsInvariants(ret))
