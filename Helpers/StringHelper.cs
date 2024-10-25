3dc0a9e54c18594348669493b8f0165981898c7e
Matthew Holmes <matthew.holmes111@gmail.com>
2024-10-25 21:59:40 +0100

    added helper class for strings

---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class StringHelper
+    {
+        public static string StripPunctuation(String s)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public static string StripOuterPunctuation(String s)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start <= end && char.IsPunctuation(s[start]))
+                start++;
+
+            while (end >= start && char.IsPunctuation(s[end]))
+                end--;
+
+            return s.Substring(start, end - start + 1);
+        }
+    }
+}
