ca5651c5f32bbc4433c5529c0d6cb78188ec047e
Matthew <matthew.holmes111@gmail.com>
2025-02-19 21:22:07 +0000

    Serilog logging on startup

--- Linguine/App.xaml.cs
+++ Linguine/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Serilog;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
 
 namespace Linguine
 {
@@ -13,5 +16,18 @@ namespace Linguine
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File($"logs/log-{DateTime.Now:yyyyMMdd_HHmmss}.txt") // New file for each app start
+                .CreateLogger();
+
+            Log.Information("WPF Application Started");
+        }
     }
 }
