ac2a4df1df14674e44833a57e4e9c1987fb807f3
Matthew <matthew.holmes111@gmail.com>
2025-03-06 19:23:01 +0000

    started work on a refactor to use strongly typed config

--- Infrastructure/Config/Config.cs
+++ Infrastructure/Config/Config.cs
@@ -1,103 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-
-    // TODO - could this be a record??
-
-    [JsonObject(MemberSerialization.Fields)]
-    internal class Config // keep internal: should not be able to instantiate in wider code
+    public class Config
     {
-        internal string OpenAI_APIKeyLocation;
-        public string DeepSeek_APIKeyLocation;
+        public APIKeysConfig                    APIKeys           { get; set; } = new();
+        public LanguageConfig                   Languages         { get; set; } = new();
+        public Dictionary<LanguageCode, string> ConnectionStrings { get; set; } = new();
+    }
 
+    public class APIKeysConfig
+    {
+        public string OpenAI_APIKeyLocation   { get; set; } = string.Empty;
+        public string DeepSeek_APIKeyLocation { get; set; } = string.Empty;
+    }
 
-        internal Dictionary<LanguageCode, String> ConnectionStrings = new Dictionary<LanguageCode, string>();
-
-        [JsonConverter(typeof(StringEnumConverter))]
-        internal LanguageCode NativeLanguage;
-        [JsonConverter(typeof(StringEnumConverter))]
-        internal LanguageCode TargetLanguage;
-
-
-        internal Dictionary<LanguageCode, LearnerLevel> LearnerLevels =
-            new Dictionary<LanguageCode, LearnerLevel>();
-
-
-        internal Config Copy()
-        {
-            Config copy = new Config
-            {
-                NativeLanguage = this.NativeLanguage,
-                TargetLanguage = this.TargetLanguage,
-
-                OpenAI_APIKeyLocation   = this.OpenAI_APIKeyLocation,
-                DeepSeek_APIKeyLocation = this.DeepSeek_APIKeyLocation,
-
-                ConnectionStrings = new Dictionary<LanguageCode, String>(),
-                LearnerLevels = new Dictionary<LanguageCode, LearnerLevel>()
-
-            };
-
-            if (this.ConnectionStrings is not null)
-            {
-                foreach (var entry in this.ConnectionStrings)
-                {
-                    copy.ConnectionStrings.Add(entry.Key, entry.Value);
-                }
-            }
-
-            if (this.LearnerLevels is not null)
-            {
-                foreach (var entry in this.LearnerLevels)
-                {
-                    copy.LearnerLevels.Add(entry.Key, entry.Value);
-                }
-            }
-
-            return copy;
-        }
-
-        internal bool Equals(Config rhs)
-        {
-            if (rhs == null)
-            {
-                return false;
-            }
-
-            bool sameTarget = TargetLanguage == rhs.TargetLanguage;
-            bool sameNative = NativeLanguage == rhs.NativeLanguage;
-
-            bool sameOAIAPIKeyLocation = OpenAI_APIKeyLocation   == rhs.OpenAI_APIKeyLocation;
-            bool sameDSAPIKeyLocation = DeepSeek_APIKeyLocation == rhs.DeepSeek_APIKeyLocation;
-
-            bool sameAPIKeyLocation = sameDSAPIKeyLocation && sameDSAPIKeyLocation;
-
-            bool sameConnectionStrings = ConnectionStrings.Keys.Count == rhs.ConnectionStrings.Keys.Count;
-
-            foreach (var entry in this.ConnectionStrings)
-            {
-                sameConnectionStrings = sameConnectionStrings && (ConnectionStrings[entry.Key] == rhs.ConnectionStrings[entry.Key]);
-            }
-
-            bool sameLevels = LearnerLevels.Keys.Count == rhs.LearnerLevels.Keys.Count;
-
-            foreach (var entry in this.LearnerLevels)
-            {
-                sameLevels = sameLevels && (LearnerLevels[entry.Key] ==
-                    rhs.LearnerLevels[entry.Key]);
-            }
-
-            return sameTarget && sameNative && sameLevels && sameAPIKeyLocation && sameConnectionStrings;
-
-        }
-
+    public class LanguageConfig
+    {
+        public LanguageCode                           NativeLanguage { get; set; }
+        public LanguageCode                           TargetLanguage { get; set; }
+        public Dictionary<LanguageCode, LearnerLevel> LearnerLevels  { get; set; } = new();
     }
 }
