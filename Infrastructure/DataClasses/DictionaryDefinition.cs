5dd3fa69882e378c33c63f4c1c5fc9f2ebc7732b
Matthew <matthew.holmes111@gmail.com>
2025-04-02 16:59:23 +0100

    added properties for definition pronunciations

--- Infrastructure/DataClasses/DictionaryDefinition.cs
+++ Infrastructure/DataClasses/DictionaryDefinition.cs
@@ -15,5 +15,8 @@ namespace Infrastructure
         public String Word { get; set; }
         public String Definition { get; set; }
         public String Source { get; set; }
+        public String? IPAPronunciation { get; set; }
+        public String? RomanisedPronuncation { get; set; }
+
     }
 }
