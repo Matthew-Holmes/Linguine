033b937f5b36df40c4a940257df930ded6926aff
Matthew <matthew.holmes111@gmail.com>
2025-04-06 17:54:36 +0100

    changed a threshold

--- Learning/LearningTacticsRepository/EventuallyLearnt/EventuallyLearntSlowly.cs
+++ Learning/LearningTacticsRepository/EventuallyLearnt/EventuallyLearntSlowly.cs
@@ -12,7 +12,7 @@ namespace Learning.LearningTacticsRepository
 
         public EventuallyLearntSlowly()
         {
-            Thresholds = new List<BasicThresholds> { new BasicThresholds(MinTotalTime: TimeSpan.FromSeconds(60)) };
+            Thresholds = new List<BasicThresholds> { new BasicThresholds(MinTotalTime: TimeSpan.FromSeconds(120)) };
         }
     }
 }
