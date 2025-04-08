96ca8c3de8064fe0f206d74796f7303f3d8fe402
Matthew <matthew.holmes111@gmail.com>
2025-04-08 13:58:17 +0100

    Tactician can compute rewards available

--- Learning/Tactics/Tactician.cs
+++ Learning/Tactics/Tactician.cs
@@ -1,5 +1,7 @@
 ﻿using DataClasses;
+using Learning.Strategy;
 using Learning.Tactics;
+using MathNet.Numerics;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 using System.Collections.Generic;
@@ -15,9 +17,47 @@ namespace Learning
         private Strategist Strategist { get; init; }
         private MarkovGraph MarkovGraph { get; set; }
 
+        private double LookAheadDays { get; set; }
+
         public Tactician(Strategist strat)
         {
             Strategist = strat;
         }
-}
+
+        public IReadOnlyDictionary<Type, double> GetRewardAtTerminationFor(int defKey)
+        {
+            if (!Strategist.LastTacticUsedForDefinition.ContainsKey(defKey))
+            {
+                return Strategist.DefaultRewards; // uses population averages
+            }
+
+            double currentReward = Strategist.GetExistingPKnown(defKey, LookAheadDays);
+
+            FollowingSessionDatum? input = Strategist.GetCurrentRewardFeatures(defKey, LookAheadDays);
+
+            if (input is null)
+            {
+                throw new Exception("requested reward for definition without observed sessions");
+            }
+
+            Dictionary<Type, double> ret = new Dictionary<Type, double>();
+
+            foreach (Type tacticType in Strategist.TacticsUsed)
+            {
+                // predict the probability we'll know it in LookAheadDays
+                // if tacticType tactic is deployed
+
+                FollowingSessionDatum newInput = input with
+                    {
+                        sessionTacticType = tacticType,
+                        intervalDays = LookAheadDays
+                    };
+
+                double newReward = Strategist.PredictProbability(newInput);
+
+                ret[tacticType] = newReward;
+            }
+            return ret.AsReadOnly();
+        }
+    }
 }
