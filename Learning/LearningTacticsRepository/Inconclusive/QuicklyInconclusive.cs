47d99132487cea42ed0f5925ab91e526134f9219
Matthew <matthew.holmes111@gmail.com>
2025-04-09 11:38:22 +0100

    added new tactic to open up the mdp a bit more

---
+++ Learning/LearningTacticsRepository/Inconclusive/QuicklyInconclusive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.LearningTacticsRepository
+{
+    class QuicklyInconclusive : LearningTactic
+    {
+        public override LearningTactic? Prerequisite => new Inconclusive();
+
+        public QuicklyInconclusive()
+        {
+            Thresholds = new List<BasicThresholds> { new BasicThresholds(MaxTotalTime: TimeSpan.FromSeconds(20)) };
+        }
+    }
+}
