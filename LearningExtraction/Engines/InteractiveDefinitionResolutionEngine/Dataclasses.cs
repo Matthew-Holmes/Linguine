8373d30fb22f4e9ccc040ee56d85a9dd71f399fb
Matthew <matthew.holmes111@gmail.com>
2025-04-10 16:08:57 +0100

    laid out new engine for interactive definition resolution

---
+++ LearningExtraction/Engines/InteractiveDefinitionResolutionEngine/Dataclasses.cs
@@ -0,0 +1,11 @@
+﻿using DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningExtraction
+{
+    public record InitialDefinitionAnalyis(String wordBestTranslation, String meaning);
+}
---
+++ LearningExtraction/Engines/InteractiveDefinitionResolutionEngine/InteractiveDefinitionResolutionEngine.cs
@@ -0,0 +1,49 @@
+﻿using DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningExtraction
+{
+    public interface ICanResolveDefinitions 
+    {
+        public bool ResolveFromExisting(Statement stat, int defIndex, DictionaryDefinition def);
+        public bool ResolveFromNew(     Statement stat, int defIndex, DictionaryDefinition newDef);
+
+
+        public Task<StatementTranslation>        GetTranslation(Statement stat);
+        public Task<InitialDefinitionAnalyis>    GetInitialAnalysis(   Statement stat, int defIndex);
+        public Task<DictionaryDefinition>        GenerateNewDefinition(Statement stat, int defIndex);
+    }
+
+
+    class InteractiveDefinitionResolutionEngine : ICanResolveDefinitions
+    {
+        public Task<DictionaryDefinition> GenerateNewDefinition(Statement stat, int defIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InitialDefinitionAnalyis> GetInitialAnalysis(Statement stat, int defIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StatementTranslation> GetTranslation(Statement stat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResolveFromExisting(Statement stat, int defIndex, DictionaryDefinition def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResolveFromNew(Statement stat, int defIndex, DictionaryDefinition newDef)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
