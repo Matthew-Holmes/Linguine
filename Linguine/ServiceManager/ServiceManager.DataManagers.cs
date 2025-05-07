71a275245e1658f2018e09979391cdb1aacab2a1
Matthew <matthew.holmes111@gmail.com>
2025-05-07 15:03:05 +0100

    more scaffolding

---
+++ Linguine/ServiceManager/ServiceManager.DataManagers.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace Linguine
+{
+    partial class ServiceManager
+    {
+
+        // new interface should look something like this?
+
+        //public DataManagerBase<T> GetManager()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ Linguine/ServiceManager/ServiceManager.Engines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguine
+{
+
+    public enum EngineState
+    {
+        NotYetStarted,
+        Building,
+        Built,
+        Stale,
+        Disposing,
+        Disposed,
+    }
+
+
+    partial class ServiceManager
+    {
+        // have the engines as properties??
+    }
+}
