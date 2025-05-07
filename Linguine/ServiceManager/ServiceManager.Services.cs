d72db42503a85adc963021ba1619001b23a8d383
Matthew <matthew.holmes111@gmail.com>
2025-05-07 14:52:27 +0100

    scaffolding

---
+++ Linguine/ServiceManager/ServiceManager.Services.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguine
+{
+    internal enum ServiceState
+    {
+        NotStarted,
+        Loading,
+        Loaded,
+        Failed,
+        ShuttingDown,
+        Stopped
+    }
+
+
+    partial class ServiceManager
+    {
+        // services
+        // lazy/background loading 
+        // service state - notStarted, loading, loaded, failed, shuttingDown, stopped
+        // nullable - if null then the model can query the state?
+        // track when a distribution is out of date - new data loaded
+    }
+}
---
+++ Linguine/ServiceManager/ServiceManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguine
+{
+    internal partial class ServiceManager
+    {
+        // engines
+        // services
+        // data access
+
+        // services
+        // lazy/background loading 
+        // service state - notStarted, loading, loaded, failed, shuttingDown, stopped
+        // nullable - if null then the model can query the state?
+        // track when a distribution is out of date - new data loaded
+        
+        // data
+        // manage caching??
+        // how to avoid layer hell?
+        // should we put the context factory here? 
+
+        // engines
+        // read only database handles??
+        // main model orchestrates engine with data saving
+        // why does learning extraction need Infra
+        // can we make its functions only require readonly handles
+
+
+        // Dispose
+        // should clean up all resources
+
+    }
+}
