7847c2777670ef6ad8c74b9cf1917047c0053cb7
Matthew <matthew.holmes111@gmail.com>
2025-05-12 12:11:08 +0100

    ServiceManager holds definition learning service

--- Linguine/ServiceManager/ServiceManager.Services.cs
+++ Linguine/ServiceManager/ServiceManager.Services.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Config;
+using DataClasses;
+using Infrastructure;
+using Learning;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Linguine
 {
-    internal enum ServiceState
+    public enum ServiceState
     {
         NotStarted,
         Loading,
@@ -17,6 +22,12 @@ namespace Linguine
     }
 
 
+    public class Services
+    {
+        public IDefinitionLearningService? DefLearning      { get; set;  } = null;
+        public ServiceState                DefLearningState { get; init; } = ServiceState.NotStarted;
+    }
+
     partial class ServiceManager
     {
         // services
@@ -24,5 +35,50 @@ namespace Linguine
         // service state - notStarted, loading, loaded, failed, shuttingDown, stopped
         // nullable - if null then the model can query the state?
         // track when a distribution is out of date - new data loaded
+
+        public Services Services { get; init; } = new Services();
+
+
+        public void InitialiseServices()
+        {
+            //IDefinitionLearningService dl = DefinitionLearningServiceFactory.BuildDLS();
+        }
+
+        internal void StartDefinitionLearningService()
+        {
+            if (ManagerState != DataManagersState.Initialised)
+            {
+                throw new Exception("managers not loaded yet");
+            }
+
+            if (Services.DefLearningState is ServiceState.Loading)
+            {
+                return; // TODO - should we be waiting here??
+            }
+
+            if (Services.DefLearningState is not ServiceState.NotStarted)
+            {
+                throw new Exception("already started, should be using restart");
+            }
+
+            if (Engines.DefFrequenciesState is EngineState.NotYetStarted)
+            {
+                StartDefinitionFrequencyEngine();
+            }
+
+            while (Engines.DefFrequenciesState is EngineState.Building)
+            {
+                Thread.Sleep(5);
+            }
+            
+            using var context = DBF.CreateDbContext();
+
+            FrequencyData freqData = Engines.DefFrequencies!.ComputeFrequencyData(context);
+
+            List<TestRecord>? allRecords = Managers!.TestRecords.AllRecordsTimeSorted();
+
+            Services.DefLearning = DefinitionLearningServiceFactory.BuildDLS(freqData, allRecords, ConfigManager.CancelRunningTasksSource);
+        }
+
     }
 }
