e71e22e871b4a9b41664e16f9b61b163454786eb
Matthew <matthew.holmes111@gmail.com>
2025-05-12 11:37:34 +0100

    ServiceManager holds a definition frequency engine

--- Linguine/ServiceManager/ServiceManager.Engines.cs
+++ Linguine/ServiceManager/ServiceManager.Engines.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure;
+using Learning;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +14,46 @@ namespace Linguine
         NotYetStarted,
         Building,
         Built,
-        Stale,
-        Disposing,
-        Disposed,
+        //Stale, // do we want something like this??
     }
 
+    // single task engines for achieving specific data processing/generation
+
+    public class Engines
+    {
+        public IDefinitionFrequencyEngine? DefFrequencies { get; set; } = null;
+        public EngineState DefFrequenciesState { get; set; } = EngineState.NotYetStarted;
+    }
 
     partial class ServiceManager
     {
         // have the engines as properties??
+
+        public Engines Engines { get; init; } = new Engines();
+
+        public void StartDefinitionFrequencyEngine()
+        {
+            if (Engines.DefFrequenciesState is not EngineState.NotYetStarted)
+            {
+                throw new Exception("already started, should be using restart");
+            }
+
+            if (Engines.DefFrequenciesState is EngineState.Building)
+            {
+                return;
+            }
+
+            Engines.DefFrequenciesState = EngineState.Building;
+
+            Engines.DefFrequencies = DefinitionFrequencyEngineFactory.BuildDefinitionFrequencyEngine();
+
+            Engines.DefFrequenciesState = EngineState.Built;
+        }
+
+
+        // TODO - cache the result from def frequency engine??
+        // may not need both
+        // Statement engine
+        // Definition frequency engine
     }
 }
