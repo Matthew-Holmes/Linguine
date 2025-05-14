6971dcaa67c709e28cbfc091f629cc6098c04db7
Matthew <matthew.holmes111@gmail.com>
2025-05-14 16:31:20 +0100

    xaml for definition repair tab

---
+++ Linguine/Helpers/EditMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguine.Helpers
+{
+    public enum EditMethod
+    {
+        NotEdited,
+        MachineEdited,
+        UserEdited
+    }
+}
---
+++ Linguine/Tabs/WPF/Converters/EditMethodToBackgroundConverter.cs
@@ -0,0 +1,41 @@
+﻿using Linguine.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Linguine.Tabs.WPF
+{
+    public class EditMethodToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is EditMethod em)
+            {
+                switch (em)
+                {
+                    case EditMethod.NotEdited:
+                        return Brushes.White;
+                    case EditMethod.MachineEdited:
+                        return Brushes.LightGray;
+                    case EditMethod.UserEdited:
+                        return Brushes.LightBlue;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+
+            throw new ArgumentException("should only pass EditMethod enum to this");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
