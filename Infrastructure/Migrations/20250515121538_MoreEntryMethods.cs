a23bab57da13e7fd69294c66e8e9fa6236409aa9
Matthew <matthew.holmes111@gmail.com>
2025-05-15 13:17:19 +0100

    migration

---
+++ Infrastructure/Migrations/20250515121538_MoreEntryMethods.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class MoreEntryMethods : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParsedDefinitionEntryMethod",
+                table: "ParsedDictionaryDefinitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DefinitionEntryMethod",
+                table: "DictionaryDefinitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IPAEntryMethod",
+                table: "DictionaryDefinitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RomanisedEntryMethod",
+                table: "DictionaryDefinitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParsedDefinitionEntryMethod",
+                table: "ParsedDictionaryDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "DefinitionEntryMethod",
+                table: "DictionaryDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "IPAEntryMethod",
+                table: "DictionaryDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "RomanisedEntryMethod",
+                table: "DictionaryDefinitions");
+        }
+    }
+}
