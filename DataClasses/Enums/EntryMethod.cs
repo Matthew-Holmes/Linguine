0ae8dde13c67e694e0ded8c58787ef048c9524fb
Matthew <matthew.holmes111@gmail.com>
2025-05-15 13:13:41 +0100

    added more entry methods

--- DataClasses/Enums/EntryMethod.cs
+++ DataClasses/Enums/EntryMethod.cs
@@ -8,9 +8,18 @@ namespace DataClasses
 {
     public enum EntryMethod
     {
+        // initial methods
         Machine = 0,
         User = 1,
+        // when choosing from options
         UserOverwriteMachine = 2,
-        UserOverwriteUser = 3
+        UserOverwriteUser = 3,
+        MachineOverwriteMachine = 4,
+        MachineOverwriteUser = 5,
+        // when the data is textual
+        UserGeneratesNewMachineOverwritesMachine = 6,
+        UserGeneratesNewMachineOverwritesUser = 7,
+        UserWritesCustomOverwritesMachine = 8,
+        UserWritesCustomOverwritesUser = 9 ,
     }
 }
