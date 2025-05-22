e940620486e7a0bd13fd34659856567ccf15cbd6
Matthew <matthew.holmes111@gmail.com>
2025-05-22 11:39:15 +0100

    migration

---
+++ Infrastructure/Migrations/20250522102239_KeepDefinitionHistory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class KeepDefinitionHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreviousDefinitions",
+                table: "DictionaryDefinitions",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviousDefinitions",
+                table: "DictionaryDefinitions");
+        }
+    }
+}
