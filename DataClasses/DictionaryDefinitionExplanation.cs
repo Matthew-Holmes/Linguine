bc83128c864d5da933294e983ae456ae5a330251
Matthew <matthew.holmes111@gmail.com>
2025-05-23 16:38:19 +0100

    added specific components to definition explanations

--- DataClasses/DictionaryDefinitionExplanation.cs
+++ DataClasses/DictionaryDefinitionExplanation.cs
@@ -14,8 +14,19 @@ namespace DataClasses
         public int DictionaryDefinitionKey { get; set; }
         public DictionaryDefinition CoreDefinition { get; set; }
         public LanguageCode NativeLanguage { get; set; }
-        public String Explanation { get; set; }
-
-        public EntryMethod ExplanationEntryMethod { get; set; } = EntryMethod.Machine;
+        public String DetailedDefinition { get; set; } // may include structures that are specific to this word
+        public String Synonyms { get; set; }
+        public String DifferencesFromOtherSenses { get; set; }
+        public String Register { get; set; } // tone/register/specific to specific domain
+        public String Example1 { get; set; }
+        public String Example2 { get; set; }
+        public String Example3 { get; set; }
+        public EntryMethod DetailedDefinitionEntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod SynonymsEntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod DifferencesEntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod RegisterEntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod Example1EntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod Example2EntryMethod { get; set; } = EntryMethod.Machine;
+        public EntryMethod Example3EntryMethod { get; set; } = EntryMethod.Machine;
     }
 }
