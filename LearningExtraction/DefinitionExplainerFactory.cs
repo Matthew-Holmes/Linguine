3a70438b04493794f2f8d25610df96797e20aec3
Matthew <matthew.holmes111@gmail.com>
2025-05-24 18:06:47 +0100

    updated DefinitionExplainingEngine to use more granular information

--- LearningExtraction/DefinitionExplainerFactory.cs
+++ LearningExtraction/DefinitionExplainerFactory.cs
@@ -14,12 +14,50 @@ namespace LearningExtraction
     {
         public static ICanExplainDefinitions BuildExplanationEngine()
         {
-            AgentBase explainingAgent = AgentFactory.GenerateProcessingAgent(
-                AgentTask.DefinitionExplaining,
+            AgentBase detailedDefinitionAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.DetailedDefinitionWriting,
                 ConfigManager.Config.Languages.NativeLanguage,
                 isHighPerformance: true);
 
-            return new DefinitionExplainingEngine(explainingAgent);
+            AgentBase synonymsAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.DefinitionSynonyms,
+                ConfigManager.Config.Languages.NativeLanguage,
+                isHighPerformance: true);
+
+            AgentBase wordSenseDistinguishingAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.WordSenseDistinguishingExplanation,
+                ConfigManager.Config.Languages.NativeLanguage,
+                isHighPerformance: true);
+
+            AgentBase registerAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.RegisterAnalysis,
+                ConfigManager.Config.Languages.NativeLanguage,
+                isHighPerformance: true);
+
+            AgentBase easyExampleAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.SimpleExampleOfWord,
+                ConfigManager.Config.Languages.TargetLanguage);
+
+            AgentBase mediumExampleAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.MediumExampleOfWord,
+                ConfigManager.Config.Languages.TargetLanguage);
+
+            AgentBase hardExampleAgent = AgentFactory.GenerateProcessingAgent(
+                AgentTask.HardExampleOfWord,
+                ConfigManager.Config.Languages.TargetLanguage,
+                isHighPerformance: true);
+
+            var ret = new DefinitionExplainingEngine();
+
+            ret.DetailedAgent         = detailedDefinitionAgent;
+            ret.SynonymAgent          = synonymsAgent;
+            ret.DistinguishingAgent   = wordSenseDistinguishingAgent;
+            ret.RegisterAnalysisAgent = registerAgent;
+            ret.ExampleAgent1         = easyExampleAgent;
+            ret.ExampleAgent2         = mediumExampleAgent;
+            ret.ExampleAgent3         = hardExampleAgent;
+
+            return ret;
         }
     }
 }
--- LearningExtraction/Engines/DefinitionExplainingEngine/DefinitionExplainingEngine.cs
+++ LearningExtraction/Engines/DefinitionExplainingEngine/DefinitionExplainingEngine.cs
@@ -1,11 +1,14 @@
 ﻿using Agents;
 using DataClasses;
+using Helpers;
+using ExternalMedia;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Enumeration;
 
 namespace LearningExtraction.Engines.DefinitionExplainingEngine
 {
@@ -18,12 +21,15 @@ namespace LearningExtraction.Engines.DefinitionExplainingEngine
     }
     class DefinitionExplainingEngine : ICanExplainDefinitions
     {
-        private AgentBase ExplainingAgent { get; init; }
+        public AgentBase DetailedAgent         { get; set; }
+        public AgentBase SynonymAgent          { get; set; }
+        public AgentBase DistinguishingAgent   { get; set; }
+        public AgentBase RegisterAnalysisAgent { get; set; }
+        public AgentBase ExampleAgent1         { get; set; }
+        public AgentBase ExampleAgent2         { get; set; }
+        public AgentBase ExampleAgent3         { get; set; }
+        
 
-        public DefinitionExplainingEngine(AgentBase agent)
-        {
-            ExplainingAgent = agent;
-        }
 
         public async Task<List<DictionaryDefinitionExplanation>> ExplainDefinitions(
             List<DictionaryDefinition> definitions, LanguageCode native)
@@ -31,25 +37,49 @@ namespace LearningExtraction.Engines.DefinitionExplainingEngine
 
             List<String> prompts = FormPrompts(definitions, native);
 
-            var getResponseTasks = prompts.Select(prompt => ExplainingAgent.GetResponse(prompt));
+            var getDetailedTasks = prompts.Select(prompt => DetailedAgent.GetResponse(prompt));
+            var getSynonymsTasks = prompts.Select(prompt => SynonymAgent.GetResponse(prompt));
+            var getDisguishTasks = prompts.Select(prompt => DistinguishingAgent.GetResponse(prompt));
+            var getRegisterTasks = prompts.Select(prompt => RegisterAnalysisAgent.GetResponse(prompt));
+            var getExample1Tasks = prompts.Select(prompt => ExampleAgent1.GetResponse(prompt));
+            var getExample2Tasks = prompts.Select(prompt => ExampleAgent2.GetResponse(prompt));
+            var getExample3Tasks = prompts.Select(prompt => ExampleAgent3.GetResponse(prompt));
 
-            String[] responses = await Task.WhenAll(getResponseTasks);
 
-            if (responses.Count() != definitions.Count)
-            {
-                throw new Exception("failed to explain all the definitions");
-            }
+            await Task.WhenAll(
+                getDetailedTasks.Concat(getSynonymsTasks)
+                                .Concat(getDisguishTasks)
+                                .Concat(getRegisterTasks)
+                                .Concat(getExample1Tasks)
+                                .Concat(getExample2Tasks)
+                                .Concat(getExample3Tasks)
+            );
+
+            string[] detailedResults = getDetailedTasks.Select(t => t.Result).ToArray();
+            string[] synonymsResults = getSynonymsTasks.Select(t => t.Result).ToArray();
+            string[] disguishResults = getDisguishTasks.Select(t => t.Result).ToArray();
+            string[] registerResults = getRegisterTasks.Select(t => t.Result).ToArray();
+            string[] example1Results = getExample1Tasks.Select(t => t.Result).ToArray();
+            string[] example2Results = getExample2Tasks.Select(t => t.Result).ToArray();
+            string[] example3Results = getExample3Tasks.Select(t => t.Result).ToArray();
+
 
             List<DictionaryDefinitionExplanation> ret = new();
-            
+
 
             for (int i = 0; i != definitions.Count; i++)
             {
-                DictionaryDefinitionExplanation toAdd = new DictionaryDefinitionExplanation 
-                { 
-                    CoreDefinition = definitions[i],
-                    NativeLanguage = native,
-                    Explanation = responses[i]
+                DictionaryDefinitionExplanation toAdd = new DictionaryDefinitionExplanation
+                {
+                    CoreDefinition             = definitions[i],
+                    NativeLanguage             = native,
+                    DetailedDefinition         = detailedResults[i],
+                    Synonyms                   = synonymsResults[i],
+                    DifferencesFromOtherSenses = disguishResults[i],
+                    Register                   = registerResults[i],
+                    Example1                   = example1Results[i],
+                    Example2                   = example2Results[i],
+                    Example3                   = example3Results[i],
                 };
 
                 ret.Add(toAdd);
@@ -64,14 +94,16 @@ namespace LearningExtraction.Engines.DefinitionExplainingEngine
 
             // TODO - this is English specific - need to localise!
 
+            ForDefinitionResolution promptParts = TextFactory.DefinitionResolutionString(native);
+
             foreach (DictionaryDefinition definition in definitions)
             {
                 StringBuilder builder = new StringBuilder();
                 builder.Append('\n');
-                builder.Append("word: ");
+                builder.Append($"{promptParts.word}: ");
                 builder.Append(definition.Word);
                 builder.Append('\n');
-                builder.Append("definition: ");
+                builder.Append($"{promptParts.singleDefinition}: ");
                 builder.Append(definition.Definition);
 
                 ret.Add(builder.ToString());
